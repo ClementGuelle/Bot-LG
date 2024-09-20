6f77e2946588c6b68151c753cfa06b774afdb7a8
GUELLE Clement <153915554+betonfixe@users.noreply.github.com>
2024-09-20 20:52:04 +0200

    Envoie de message MP à l'utilisateur lorsqu'il rejoint et quitte le salon vocal

--- src/events/voice_channel/voiceStateUpdate.js
+++ src/events/voice_channel/voiceStateUpdate.js
@@ -5,32 +5,48 @@ module.exports = {
 	once: false,
 	async execute(client, oldState, newState) {
 
-
 		const embed = new MessageEmbed()
-			.setAuthor({
-				name: `${member.user.tag} (${member.id})`,
-				iconURL: member.user.displayAvatarURL(),
-			})
-			.setColor("#21ff81")
-			.setDescription(` Nom d'utilisateur: ${member}
-							± Créé le: <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
-							± Rejoint le: <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)`)
-			.setTimestamp()
-			.setFooter({ text: "L'utilisateur a rejoint!" });
+		const userChannel = await client.users.fetch(newState.id)
 
 
-		
+		if (newState.channelId === process.env.VOCAL_CHANNEL )
+		{
+			
+			embed.setAuthor({
+					name: client.user.username,
+					iconURL: client.user.displayAvatarURL(),
+				})
+				.setColor("#21ff81")
+				.setDescription(` ${userChannel}, 
+				Tu as rejoint une partie de Loup-Garou UHC. Je t'invite à respecter scrupuleusement toutes les règles écrites dans le salon dédié à cet effet, sans quoi je crains que tu ne puisses pas participer aux prochaines parties.
+			
+				Durant toute la partie, tu devras rester dans le salon "Village" en mode muet avec ton casque. Un maître du village sera présent pour répondre à tes questions.
+			
+				Tu pourras quitter le vocal à la fin de la partie.`)
+				.setFooter({ text: "Bonne partie ! Amuse-toi bien." });
 
 
-		// Envoie un message en MP à l'utilisateur quand il rejoint
-		const userChannel = client.channels.cache.get(process.env.LOG_CHANNEL);
-		userChannel.send({ embeds: [embed] });
+			// Envoie un message en MP à l'utilisateur quand il rejoint
+			userChannel.send({ embeds: [embed] });
+		}
+		else if (oldState.channelId === process.env.VOCAL_CHANNEL && newState.channelId === null)
+		{
+			embed.setAuthor({
+					name: client.user.username,
+					iconURL: client.user.displayAvatarURL(),
+				})
+				.setColor("#ff0000")
+				.setDescription(` ${userChannel}, 
+				Merci d'avoir participé à cette partie ! J'espère que tu t'es bien amusé.
 
-		// Envoie un message en MP à l'utilisateur quand il quitte 
-		userChannel.send({ embeds: [embed] });
+				Si tu as envie de nous donner ton avis sur cette partie pour nous aider à nous améliorer, n'hésite pas à taper la commande "/avis".
 
-		// Envoie un message dans les logs du serv sur ce que le bot fait 
-		const logChannel = client.channels.cache.get(process.env.LOG_CHANNEL);
-		logChannel.send({ embeds: [embed] });
+				Si tu as des questions ou si tu veux signaler quelque chose (à propos du jeu, d'un joueur ou d'un rôle), contacte les maires de la ville dans le salon FAQ.`)
+				.setFooter({ text: "Merci d'avoir participé !" });
+
+
+			// Envoie un message en MP à l'utilisateur quand il quitte
+			userChannel.send({ embeds: [embed] });
+		}
 	},
 };
